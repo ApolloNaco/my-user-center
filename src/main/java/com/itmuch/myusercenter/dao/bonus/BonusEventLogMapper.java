e312d5d56053d59b250e610faa183eaeb70c59cc
croot <cgypersonal@163.com>
2021-03-18 22:59:32 +0800

    编写Mq消费者 加积分

---
+++ src/main/java/com/itmuch/myusercenter/dao/bonus/BonusEventLogMapper.java
@@ -0,0 +1,8 @@
+package com.itmuch.myusercenter.dao.bonus;
+
+import com.itmuch.myusercenter.domain.entity.bonus.BonusEventLog;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BonusEventLogMapper extends Mapper<BonusEventLog> {
+
+}
