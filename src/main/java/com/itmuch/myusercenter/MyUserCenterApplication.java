8003caa93d247e92c3d22f02af15549adfead185
croot <cgypersonal@163.com>
2021-03-25 22:28:23 +0800

    stream-cloud-rocketmq 接受消息

--- src/main/java/com/itmuch/myusercenter/MyUserCenterApplication.java
+++ src/main/java/com/itmuch/myusercenter/MyUserCenterApplication.java
@@ -2,11 +2,14 @@ package com.itmuch.myusercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import tk.mybatis.spring.annotation.MapperScan;
 
 // 扫描mybatis哪些包里面的接口
 @MapperScan("com.itmuch.myusercenter.dao")
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class MyUserCenterApplication {
 
     public static void main(String[] args) {
